171de536f8209dc14947d22e1171153e0494c485
Fletcher Kelly <fletcher_kelly@outlook.com>
2021-05-19 07:15:13 +0200

    add deploy.ps1 for easier deployments

---
+++ examples/deploy.ps1
@@ -0,0 +1,17 @@
+## Place Resource Group Name here
+$rgName = ""
+## add tags if you want to add metadata
+$tags = @{"deploymentMethod"="bicep"; "Can Be Deleted"="yes"}
+## location to be deployed into
+$rgLocation = "westeurope"
+
+$rg = New-AzResourceGroup -Name $rgName -Location $rgLocation #use this command when you need to create a new resource group for your deployment
+New-AzTag -ResourceId $rg.ResourceId -Tag $tags
+
+## arm file - for testing i use password
+#New-AzResourceGroupDeployment -ResourceGroupName $rgName -TemplateFile .\azureDeploy.json -authenticationType password
+
+## bicep Deployment
+## Bicep File name
+$bicepFile = ""
+New-AzResourceGroupDeployment -ResourceGroupName $rgName -TemplateFile $bicepFile -authenticationType password
