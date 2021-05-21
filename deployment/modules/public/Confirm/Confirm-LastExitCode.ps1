da3e47d802a5b7b133cc2ecd6e21ff2d02a44a7c
Fletcher Kelly <fletcher_kelly@outlook.com>
2021-05-21 08:26:05 +0200

    move some files to development

--- deployment/modules/public/Confirm/Confirm-LastExitCode.ps1
+++
@@ -1,5 +0,0 @@
-function Confirm-LastExitCode {
-    if ($LastExitCode -ne 0) {
-        throw "An error occurred executing the previous command. Check its output for more details."
-    }
-}
--- deployment/modules/public/write-header.psm1
+++
@@ -1,8 +0,0 @@
-function Write-Header {
-    param([string] $message)
-
-    Write-Host "************************************************************************" -ForegroundColor Cyan | Out-Null
-    Write-Host $message -ForegroundColor Yellow | Out-Null
-    Write-Host "************************************************************************" -ForegroundColor Cyan | Out-Null
-    Write-Host "" | Out-Null
-}
