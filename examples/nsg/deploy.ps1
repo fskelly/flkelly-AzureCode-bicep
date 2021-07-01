57367236775a7aa5ff3bc4b259562f961489ad21
Fletcher Kelly <fletcher_kelly@outlook.com>
2021-07-01 09:47:34 +0200

    re-add nsg files

--- examples/nsg/deploy.ps1
+++ examples/nsg/deploy.ps1
@@ -1,15 +1,26 @@
-## Set Variables
-$rgName = ''
-$rgLocation = ''
+## Place Resource Group Name here
+$rgName = ""
+## add tags if you want to add metadata
+$tags = @{"deploymentMethod"="bicep"; "Can Be Deleted"="yes"}
+## location to be deployed into
+$rgLocation = "westeurope"
 
-## Create ResourceGroup if needed
-New-AzResourceGroup -Name $rgName -Location $rgLocation
+#use this command when you need to create a new resource group for your deployment
+$rg = New-AzResourceGroup -Name $rgName -Location $rgLocation 
+New-AzTag -ResourceId $rg.ResourceId -Tag $tags
 
-# bicep file name for "basic example" - this will create a storage account
-$bicepFile = '.\main.bicep'
+## arm file - for testing i use password
+#New-AzResourceGroupDeployment -ResourceGroupName $rgName -TemplateFile .\azureDeploy.json -authenticationType password
 
-# bicep file name for "advanced example" - this will use an EXISTING storage account
-$bicepFile = '.\advanced.bicep'
+## bicep Deployment
+## Bicep File name - SSH
+$bicepFile = ".\ssh.bicep"
 
-# deploy bicep file
-New-AzResourceGroupDeployment -Name 'vmssdeploy' -ResourceGroupName $rgName -TemplateFile $bicepFile 
+## Bicep File name - RDP
+$bicepFile = ".\rdp.bicep"
+
+## Bicep File name - Active Directory
+$bicepFile = ".\adds.bicep"
+
+$deploymentName = ($bicepFile).Substring(2) + "-" +(get-date -Format ddMMyyyy-hhmmss) + "-deployment"
+New-AzResourceGroupDeployment -ResourceGroupName $rgName -TemplateFile $bicepFile -name $deploymentName
