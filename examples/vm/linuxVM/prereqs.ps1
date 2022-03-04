2623d2504f61c26f76736dec08961970fb84b8a0
Fletcher Kelly <fletcher_kelly@outlook.com>
2022-03-04 08:50:52 +0000

    ssh keygen working - PS only

---
+++ examples/vm/linuxVM/prereqs.ps1
@@ -0,0 +1,18 @@
+## Create required ssh-keys
+## Requires ssh-keygen to be installed
+
+$vmName = "testvm"
+$keyLocation  = $env:USERPROFILE + "\.ssh\"
+$privateKeyName = $vmName + "-priv-key"
+$publicKeyName = $vmName + "-priv-key.pub"
+$privateKeyPath = $keyLocation + $privateKeyName
+$publicKeyPath  = $keyLocation + $publicKeyName
+
+$password = read-host "password" -AsSecureString
+
+#$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)
+#$PlainPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+
+ssh-keygen -m PEM -t rsa -b 4096 -C $vmName -f $privateKeyPath -N $password 
+
+notepad $publicKeyPath
