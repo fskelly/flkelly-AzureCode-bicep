b78b30e8d9a319fa876421f5e273a29554791e9f
Fletcher Kelly <fletcher_kelly@outlook.com>
2022-03-21 09:57:14 +0000

    working but complex example

--- vWAN/deploy.ps1
+++ vWAN/deploy.ps1
@@ -1,10 +1,9 @@
 ## add tags if you want to add metadata
 $tags = @{"deploymentMethod"="bicep"; "Can Be Deleted"="yes"}
-## location to be deployed into
-$rgLocation = "northeurope"
+$deploymentLocation = "northeurope"
 
 ## bicep Deployment
 ## Bicep File name
 $bicepFile = ".\main.bicep"
 $deploymentName = ($bicepFile).Substring(2) + "-" +(get-date -Format ddMMyyyy-hhmmss) + "-deployment"
-New-AzSubscriptionDeployment -Name $deploymentName -Location $rgLocation -Tag $tags -TemplateFile $bicepFile
+New-AzSubscriptionDeployment -Name $deploymentName -Location $deploymentLocation -Tag $tags -TemplateFile $bicepFile
