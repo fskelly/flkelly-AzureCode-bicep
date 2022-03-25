e110fca628e036d814496314f4adda86dca9a7c6
Fletcher Kelly <fletcher_kelly@outlook.com>
2022-03-25 12:40:21 +0000

    folder cleanup

--- examples/wvd/deploymentExample.ps1
+++
@@ -1,6 +0,0 @@
-$rgName = "" ## bicep-test
-$rgLocation = "" ## westeurope
-New-AzResourceGroup -Name $rgName -Location $rgLocation
-
-$templateFile = "" ## ".\main.json"
-New-AzResourceGroupDeployment -TemplateFile $templateFile -ResourceGroupName $rgName
