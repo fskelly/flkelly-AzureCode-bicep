7e93027f19ece4ea5b62190368d829b248b07135
Fletcher Kelly <fletcher_kelly@outlook.com>
2022-03-25 12:07:03 +0000

    cleanup

--- vWAN/deploy.ps1
+++
@@ -1,9 +0,0 @@
-## add tags if you want to add metadata
-$tags = @{"deploymentMethod"="bicep"; "Can Be Deleted"="yes"}
-$deploymentLocation = "northeurope"
-
-## bicep Deployment
-## Bicep File name
-$bicepFile = ".\main.bicep"
-$deploymentName = ($bicepFile).Substring(2) + "-" +(get-date -Format ddMMyyyy-hhmmss) + "-deployment"
-New-AzSubscriptionDeployment -Name $deploymentName -Location $deploymentLocation -Tag $tags -TemplateFile $bicepFile -location northeurope -vwanlocation northeurope
